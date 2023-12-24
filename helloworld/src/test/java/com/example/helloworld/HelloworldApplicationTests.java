c0d282c9a38f9b0a90edd810a48c53418ca5ea65
wrath <wrathofhorde@gmail.com>
2023-12-24 12:52:02 +0900

    remove test code

--- helloworld/src/test/java/com/example/helloworld/HelloworldApplicationTests.java
+++ helloworld/src/test/java/com/example/helloworld/HelloworldApplicationTests.java
@@ -2,26 +2,10 @@ package com.example.helloworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.example.UserDto;
-
-import lombok.experimental.var;
 
 @SpringBootTest
 class HelloworldApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
-	@GetMapping(value = "/test")
-	public UserDto test() {
-		final UserDto userDto = new UserDto();
-		userDto.setAge(12);
-		userDto.setName("Hello");
-
-		return userDto;
-	}
-
 }
