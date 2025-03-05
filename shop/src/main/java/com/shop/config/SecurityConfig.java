6e69bee0f462c457a3d8af5d1703247682987a5f
wrath <wrathofhorde@gmail.com>
2025-03-05 18:47:16 +0900

    remove csrf

--- shop/src/main/java/com/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -9,9 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+// import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+// import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+// import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +19,12 @@ public class SecurityConfig {
 
     @Autowired
     MemberService memberService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
