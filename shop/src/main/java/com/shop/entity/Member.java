7787e42c4e7186498e907d8d810e84f970c8b9a8
wrath <wrathofhorde@gmail.com>
2025-03-14 15:21:23 +0900

    member.java

--- shop/src/main/java/com/shop/entity/Member.java
+++ shop/src/main/java/com/shop/entity/Member.java
@@ -1,17 +1,10 @@
 package com.shop.entity;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import jakarta.persistence.*;
 
 import com.shop.constant.Role;
 import com.shop.dto.MemberFormDto;
